5fec43e8b4fe899b17b66af7822b8a2c662b8364
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-10-04 22:31:41 -0500

    Contacto

--- web/settings.py
+++ web/settings.py
@@ -22,6 +22,3 @@ FREEZER_DESTINATION = PROJECT_ROOT
 FREEZER_BASE_URL = "http://colombiapython.github.io/{0}".format(REPO_NAME)
 FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
                                     # will be deleted when you run the freezer
-FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
-FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
-FLATPAGES_EXTENSION = '.md'
