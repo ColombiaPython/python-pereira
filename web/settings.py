6fbc6b607e7b048156886591ff0e93aa27f01ad2
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-10-04 21:32:34 -0500

    Deploy

--- web/settings.py
+++ web/settings.py
@@ -2,7 +2,7 @@
 
 import os
 
-REPO_NAME = "flask"  # Used for FREEZER_BASE_URL
+REPO_NAME = "python-pereira"  # Used for FREEZER_BASE_URL
 DEBUG = True
 
 # Assumes the app is located in the same directory
@@ -19,7 +19,7 @@ PROJECT_ROOT = parent_dir(APP_DIR)
 FREEZER_DESTINATION = PROJECT_ROOT
 # Since this is a repo page (not a Github user page),
 # we need to set the BASE_URL to the correct url as per GH Pages' standards
-FREEZER_BASE_URL = "http://colombiapython.github.io/python-pereira/{0}".format(REPO_NAME)
+FREEZER_BASE_URL = "http://colombiapython.github.io/{0}".format(REPO_NAME)
 FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
                                     # will be deleted when you run the freezer
 FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
