b35f75283ab673b343b21147b0b4a892920fe9b9
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-10-06 13:59:56 -0500

    Eventos en index

--- web/views.py
+++ web/views.py
@@ -6,5 +6,15 @@ import json
 def home():
     with open('data/organizador.json') as data_file:    
         organizadores = json.load(data_file)
+
+    with open('data/reunion.json') as data_file:    
+        reunion = json.load(data_file)
     
-    return render_template('index.html', organizadores=organizadores)
+    return render_template('index.html', organizadores=organizadores, proximo=reunion["proximo"], anteriores=reunion["anterior"][:5])
+
+# @app.route('/eventos')
+# def eventos():
+#     with open('data/reunion.json') as data_file:    
+#         reunion = json.load(data_file)
+    
+#     return render_template('eventos.html', anteriores=reunion["anterior"])
