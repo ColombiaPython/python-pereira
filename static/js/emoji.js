7d0675d8226541e314c12e18ab70b7ed92ce5c8f
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-10-07 23:38:15 -0500

    Testing Emojieone

---
+++ static/js/emoji.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".convert-emoji").each(function() {
+        var original = $(this).html();
+        // use .shortnameToImage if only converting shortnames (for slightly better performance)
+        var converted = emojione.shortnameToImage(original);
+        console.log(emojione.shortnameToImage(original));
+        $(this).html(converted);
+    });
+});
