032073e409b6b26be4715761a9133c8d88a9835c
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-11-09 09:10:34 -0500

    2da Meetup

--- web/views.py
+++ web/views.py
@@ -4,17 +4,17 @@ import json
 
 @app.route('/')
 def home():
-    with open('data/organizador.json') as data_file:    
+    with open('data/organizador.json', encoding='utf8') as data_file:    
         organizadores = json.load(data_file)
 
-    with open('data/reunion.json') as data_file:    
+    with open('data/reunion.json', encoding='utf8') as data_file:    
         reunion = json.load(data_file)
     
     return render_template('index.html', organizadores=organizadores, proximos=reunion["proximo"], anteriores=reunion["anterior"][:(6 - len(reunion["proximo"]))])
 
 @app.route('/eventos/')
 def eventos():
-    with open('data/reunion.json') as data_file:    
+    with open('data/reunion.json', encoding='utf8') as data_file:    
         reunion = json.load(data_file)
     
     return render_template('eventos.html', proximos=reunion["proximo"], anteriores=reunion["anterior"])
